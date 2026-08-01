31dbb06ce874b318215c3e93a512fd56acb3315b
Doublejay0639 <oluwaseunjolaoso321@gmail.com>
2026-08-01 21:08:27 +0100

    completed notification logic

--- services/notificationService.js
+++ services/notificationService.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose'
 import notificationModel from '../models/notificationModel.js'
 
 
@@ -9,4 +10,43 @@ export const createNotification = async ({ userId, type, payload }) => {
     })
 
     return notification
+}
+
+export const getMyNotifications = async (userId, {page, limit, read}) => {
+    const pageNum = parseInt(page) || 1
+    const limitNum = parseInt(limit) || 10
+    const skipValue = (pageNum - 1) * limitNum
+
+    const filter = { userId , ...(read !== undefined && { read: read === 'true' })};
+
+    const notifications = await notificationModel.find(filter)
+    .sort({createdAt: -1})
+    .skip(skipValue)
+    .limit(limitNum)
+
+    const totalCount = await notificationModel.countDocuments(filter)
+
+    return [notifications, totalCount]
+}
+
+
+export const markAsRead = async (notificationId, userId) => {
+    const filter = { _id: notificationId,
+        userId: userId
+     };
+    const update = {
+        read: true
+    };
+    const options = {
+        new: true,
+        runValidators: true
+    };
+
+    const readNotification = await notificationModel.findOneAndUpdate(filter, update, options);
+
+    if (!readNotification) {
+        throw new Error("Can only mark your own notification")   
+    }
+
+    return readNotification
 }
